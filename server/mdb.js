bf2a59de0cc8141c70ae7effea7750249e64992e
ChazzitUP <hamzarezgui.education@gmail.com>
2024-01-11 19:18:27 +0100

    all changes

--- server/mdb.js
+++ server/mdb.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 module.exports = async () => {
-    try {
-        const connectionParams = {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useUnifiedTopology: true
-        }
-        await mongoose.connect("mongodb://localhost/todolist-app",connectionParams)
-        console.log("connected to database")
-    } catch (error) {
-        console.log("could not connect to database", error)
-    }
-}
+  try {
+    const connectionParams = {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    };
+    await mongoose.connect(`${process.env.DB_URI}`, connectionParams);
+    console.log("connected to database");
+  } catch (error) {
+    console.log("could not connect to database", error);
+  }
+};
